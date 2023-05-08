b4e39b7d7b9228e86421f16f2db1deb5f68aa436
owusugyanbennet <126066201+owusugyanbennet@users.noreply.github.com>
2023-05-08 14:39:40 +0000

    initial commit

---
+++ Empee/GROUP9_300F/account.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  // Validation code here
+  
+  // Code to submit the form to the server here
+});
